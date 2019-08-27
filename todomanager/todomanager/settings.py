70e0d8570455bc2ace45b2bf6ed1bf97ad5ac4b5
shibasab <34082145+shibasab@users.noreply.github.com>
2019-08-27 16:00:54 +0900

    fixed

--- todomanager/todomanager/settings.py
+++ todomanager/todomanager/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", ".herokuapp.com"]
 
 
 # Application definition
