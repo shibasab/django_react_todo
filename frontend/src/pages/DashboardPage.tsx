0eac73f1354cc96e73729ae8058022511855f0cf
Yota Arakawa <yakyu.hira89@outlook.jp>
2026-01-09 22:46:50 +0900

    ダッシュボードでTodoコンポーネントを使うよう変更

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,5 +1,7 @@
 import { Fragment, useEffect } from 'react'
 
+import { TodoForm } from '../components/todo/TodoForm'
+import { TodoList } from '../components/todo/TodoList'
 import { useTodo } from '../hooks/useTodo'
 
 // TODO: Phase 4でlayouts/PrivateLayout.tsxにLoading表示と認証チェックを移動する
@@ -17,38 +19,8 @@ export const DashboardPage = () => {
 
   return (
     <Fragment>
-      {/* TODO: Phase 3でTodoListとTodoFormコンポーネントを作成して置き換え */}
-      <div>
-        <h2>TODO Dashboard</h2>
-        <p>Todos count: {todos.length}</p>
-        {/* 仮実装: 後でTodoListコンポーネントに置き換え */}
-        <ul>
-          {todos.map((todo) => (
-            <li key={todo.id}>
-              {todo.name} - {todo.detail}
-              <button onClick={() => removeTodo(todo.id)}>Delete</button>
-            </li>
-          ))}
-        </ul>
-        {/* 仮実装: 後でTodoFormコンポーネントに置き換え */}
-        <div>
-          <h3>Add Todo</h3>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault()
-              const formData = new FormData(e.currentTarget)
-              const name = formData.get('name') as string
-              const detail = formData.get('detail') as string
-              addTodo(name, detail)
-              e.currentTarget.reset()
-            }}
-          >
-            <input name="name" placeholder="Name" required />
-            <input name="detail" placeholder="Detail" required />
-            <button type="submit">Add</button>
-          </form>
-        </div>
-      </div>
+      <TodoList todos={todos} onDelete={removeTodo} />
+      <TodoForm onSubmit={addTodo} />
     </Fragment>
   )
 }
