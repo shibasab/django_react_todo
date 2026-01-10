0f839aa778290f79cc0438da7d01361936ac7ffa
Yota Arakawa <yakyu.hira89@outlook.jp>
2026-01-10 22:58:17 +0900

    format

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,9 +4,7 @@ import App from './components/App'
 
 const rootElement = document.getElementById('app')
 if (rootElement == null) {
-  throw new Error(
-    'Failed to find the root element with id "app". Please check your HTML file.'
-  )
+  throw new Error('Failed to find the root element with id "app". Please check your HTML file.')
 }
 
 const root = ReactDOM.createRoot(rootElement)
