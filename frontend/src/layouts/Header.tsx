a1afea20be52b14951328504fd1aac8899a984a2
Yota Arakawa <yakyu.hira89@outlook.jp>
2026-01-11 22:53:16 +0900

    Header.tsxをlayouts配下に移動

--- frontend/src/layouts/Header.tsx
+++ frontend/src/layouts/Header.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router'
 
-import { useAuth } from '../../hooks/useAuth'
+import { useAuth } from '../hooks/useAuth'
 
 export const Header = () => {
   const { isAuthenticated, user, logout } = useAuth()
