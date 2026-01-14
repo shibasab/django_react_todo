5af3fdece17fa5b4f3aad11b677a473009e94033
Yota Arakawa <yakyu.hira89@outlook.jp>
2026-01-14 14:23:36 +0900

    tailwindcssを導入

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 
+import './styles/index.css'
 import App from './App'
 
 const rootElement = document.getElementById('app')
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
   },
